96224e1af9870431b70ef962c3c92e21e7cc99fa
SuhasManju <suhas200124@gmail.com>
2025-04-29 23:17:36 +0530

    New standing logic

--- Standing/views.py
+++ Standing/views.py
@@ -1,99 +1,51 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
-from RacePulse.utils import CREATE_REQUEST, trim_decimal_zeros
+from RacePulse.utils import CREATE_REQUEST
 from Race.models import *
-from django.db.models import Sum, F, Window
 import pandas as pd
-import numpy as np
+import copy
+
 
 class CurrentStandingView(View):
     API_RESPONSE = True
-    template = ""
-
-    @CREATE_REQUEST
-    def get(self, request, *args, **kwargs):
-        context_dict = {}
-        year = request.TLPOST.get('year')
-
-        driver_standings_qs = SeasonDriverStanding.objects.filter(
-            year=year).select_related('driver').order_by('position_number')
-
-        team_standing_qs = SeasonConstructorStanding.objects.filter(
-            year=year).select_related('constructor').order_by('position_number')
-
-        engine_standing_qs = SeasonConstructorStanding.objects.filter(year=year).values(
-            'engine_manufacturer', 'engine_manufacturer__name').annotate(c_points=Sum('points')).order_by('-c_points')
-
-        driver_data = []
-        for d in driver_standings_qs:
-            i = {
-                'position': d.position_number,
-                'driverName': d.driver.name,
-                'points': trim_decimal_zeros(d.points),
-            }
-            driver_data.append(i)
-
-        context_dict['driverData'] = driver_data
-
-        team_data = []
-        for c in team_standing_qs:
-            i = {
-                'position': c.position_number,
-                'teamName': c.constructor.name,
-                'points': trim_decimal_zeros(c.points),
-            }
-            team_data.append(i)
-
-        context_dict['teamData'] = team_data
-
-        engine_data = []
-        for postion, e in enumerate(engine_standing_qs):
-            i = {
-                'postion': postion,
-                'engineName': e['engine_manufacturer__name'],
-                'points': trim_decimal_zeros(e['c_points']),
-            }
-            engine_data.append(i)
-
-        context_dict['engineData'] = engine_data
-
-        if self.API_RESPONSE:
-            return JsonResponse(context_dict)
-
-
-class CurrentStandingGraphView(View):
-    API_RESPONSE = True
-    TEMpdATE = ""
+    TEMPLATE = ""
 
     @CREATE_REQUEST
     def get(self, request, *args, **kwargs):
         context_dict = {}
 
         year = request.TLPOST.get('year')
-        columns = ['driver_id', 'driver__name', 'c_points',
+        columns = ['driver_id', 'driver__name', 'race_points', 'constructor_id', 'constructor__name',
                    'race__grand_prix__abbreviation', 'race_id', 'driver__abbreviation']
 
         driver_standing_qs = RaceData.objects.filter(
             race__year=year, type__in=[RaceData.SPRINT_RACE_RESULT, RaceData.RACE_RESULT])
-        driver_standing_qs = driver_standing_qs.annotate(c_points=Window(expression=Sum(
-            'race_points'), order_by=F('race_id').asc(), partition_by=[F('driver_id')]))
         driver_standing_qs = driver_standing_qs.values_list(*columns)
         driver_standing_qs = list(driver_standing_qs)
 
-        # Since a event can have both race and sprint we are adding them together
-        driver_df = pd.DataFrame(driver_standing_qs, columns=columns)
-        driver_df['c_points'] = driver_df['c_points'].replace(np.nan, 0)
+        # Reason for computing everything in API is due to follow reason
+        # -  Since a event can have both race and sprint we are adding them together
+        # - We would need two queries for team and drivers
+        # - max size of dataframe would be 32 * 20 = 640
+        df = pd.DataFrame(driver_standing_qs, columns=columns)
+        df['points'] = df['race_points'].fillna(0).astype("double")
+        team_df = copy.deepcopy(df)
 
-        driver_df.drop_duplicates(
-            ['driver_id', 'driver__name', 'race__grand_prix__abbreviation', 'race_id', 'driver__abbreviation'], keep='last')
+        df['c_points'] = df.groupby('driver_id')['points'].cumsum()
+        team_df['c_points'] = team_df.groupby('constructor_id')[
+            'points'].cumsum()
+
+        df = df.drop_duplicates(['driver_id', 'driver__name', 'race__grand_prix__abbreviation',
+                                'race_id', 'driver__abbreviation'], keep='last')
+        team_df = team_df.drop_duplicates(
+            ['constructor_id', 'constructor__name', 'race__grand_prix__abbreviation', 'race_id'], keep='last')
 
         driver_data = []
-        for driver_id, points in driver_df.groupby('driver_id')['c_points'].max().sort_values(ascending=False).items():
-            driver_points_list = driver_df[driver_df['driver_id'] == driver_id][[
+        for driver_id, points in df.groupby('driver_id')['c_points'].max().sort_values(ascending=False).items():
+            driver_points_list = df[df['driver_id'] == driver_id][[
                 'race__grand_prix__abbreviation', 'c_points']].values.tolist()
-            driver_info = driver_df[driver_df['driver_id']
-                                    == driver_id].iloc[0].to_dict()
+            driver_info = df[df['driver_id'] == driver_id].iloc[0].to_dict()
             i = {
                 'driverName': driver_info['driver__name'],
                 'points': points,
@@ -103,19 +55,20 @@ class CurrentStandingGraphView(View):
 
         context_dict['driverData'] = driver_data
 
-        engine_standing_qs = SeasonConstructorStanding.objects.filter(year=year).values(
-            'engine_manufacturer', 'engine_manufacturer__name').annotate(c_points=Sum('points')).order_by('-c_points')
-
-        engine_data = []
-        for postion, e in enumerate(engine_standing_qs):
+        constructor_data = []
+        for constructor_id, points in team_df.groupby('constructor_id')['c_points'].max().sort_values(ascending=False).items():
+            team_points_list = team_df[team_df['constructor_id'] == constructor_id][[
+                'race__grand_prix__abbreviation', 'c_points']].values.tolist()
+            team_info = team_df[team_df['constructor_id']
+                                == constructor_id].iloc[0].to_dict()
             i = {
-                'postion': postion,
-                'engineName': e['engine_manufacturer__name'],
-                'points': trim_decimal_zeros(e['c_points']),
+                'teamName': team_info['constructor__name'],
+                'points': points,
+                'pointsCumSum': team_points_list,
             }
-            engine_data.append(i)
+            constructor_data.append(i)
 
-        context_dict['engineData'] = engine_data
+        context_dict['teamData'] = constructor_data
 
         if self.API_RESPONSE:
             return JsonResponse(context_dict)
