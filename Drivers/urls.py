dc8923d94630a2f95687e20d3f8fbf22846864e1
SuhasManju <suhas200124@gmail.com>
2025-05-28 22:56:49 +0530

    #11 Driver Page Initial Done

--- Drivers/urls.py
+++ Drivers/urls.py
@@ -3,4 +3,6 @@ from Drivers.views import *
 
 urlpatterns = [
     path("<int:year>/", DriverView.as_view(), name="drivers"),
+    path("<str:driverId>/", DriverDetailedView.as_view(),
+         name="drivers_detailed_view"),
 ]
--- Drivers/views.py
+++ Drivers/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.http import JsonResponse
 from Race.models import *
 from RacePulse.utils import CREATE_REQUEST, trim_decimal_zeros
+from dateutil.relativedelta import relativedelta
 
 
 class DriverView(View):
@@ -26,6 +27,50 @@ class DriverView(View):
             driver_list.append(temp)
 
         context_dict['driverData'] = driver_list
+
+        if self.API_RESPONSE:
+            return JsonResponse(context_dict)
+
+        return render(request, self.TEMPLATE, context_dict)
+
+
+class DriverDetailedView(View):
+    API_RESPONSE = False
+    TEMPLATE = "drivers/drivers.html"
+
+    @CREATE_REQUEST
+    def get(self, request, *args, **kwargs):
+        context_dict = {}
+        driver_id = request.TLPOST.get("driverId")
+
+        driver = Driver.objects.select_related(
+            "country_of_birth_country", "nationality_country").get(pk=driver_id)
+        current_team = RaceData.objects.filter(
+            driver_id=driver_id).order_by("-race_id").select_related("constructor").first()
+        today = datetime.now()
+        age_diff = relativedelta(today, driver.date_of_birth)
+
+        driver_data = {
+            "id": driver_id,
+            "name": driver.name,
+            "fullName": driver.full_name,
+            "dob": driver.date_of_birth,
+            "age": f"{age_diff.years} Years and {age_diff.months} Months",
+            "birthPlace": driver.place_of_birth + ", " + driver.country_of_birth_country.name,
+            "nationality": driver.nationality_country.name,
+            "noOfChampionShip": driver.total_championship_wins,
+            "noOfWins": driver.total_race_wins,
+            "noOfPoles": driver.total_pole_positions,
+            "highestChampionshipPos": driver.best_championship_position,
+            "highestPosFinish": driver.best_race_result,
+            "highestPosStart": driver.best_starting_grid_position,
+            "driverNumber": driver.permanent_number,
+            "noOfPodiums": driver.total_podiums,
+            "currentTeam": current_team.constructor.name,
+            "noOfRaces": driver.total_race_starts,
+            "driverImg": driver.driver_img,
+        }
+        context_dict['driverData'] = driver_data
 
         if self.API_RESPONSE:
             return JsonResponse(context_dict)
