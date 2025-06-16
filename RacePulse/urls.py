e297a6b06a65172998d8224c7b0f6dad3173a207
SuhasManju <suhas200124@gmail.com>
2025-06-16 23:04:50 +0530

    #15 Home Page backend

--- RacePulse/urls.py
+++ RacePulse/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from RacePulse.views import SearchView
+from RacePulse.views import SearchView, HomeView
 
 urlpatterns = [
     path("e@am!nat!0n/", admin.site.urls),
@@ -10,5 +10,6 @@ urlpatterns = [
     path("drivers/", include("Drivers.urls")),
     path("circuits/", include("Circuit.urls")),
     path("search/", SearchView.as_view(), name="webSearch"),
+    path("", HomeView.as_view(), name="home"),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
--- RacePulse/views.py
+++ RacePulse/views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.db.models import Q
+from django.views import View
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.db.models import Q, Subquery, F, OrderBy
+from django.utils import timezone
+from django.conf import settings
 from RacePulse.utils import CREATE_REQUEST
-from Race.models import Driver, Constructor, Circuit
-
+from Race.models import Driver, Constructor, Circuit, Race, RaceData, SeasonDriverStanding, SeasonConstructorStanding
+from RacePulse.utils import trim_decimal_zeros
 
 class SearchView(APIView):
 
@@ -55,3 +60,92 @@ class SearchView(APIView):
         context_dict["circuits"] = circuit_list
 
         return Response(context_dict)
+
+
+class HomeView(View):
+    API_RESPONSE = True
+    TEMPLATE = "index.html"
+
+    @CREATE_REQUEST
+    def get(self, request, *args, **kwargs):
+        context_dict = {}
+
+        current_time = timezone.now()
+        latest_race_pk = RaceData.objects.order_by(
+            '-race_id').values_list('race_id', flat=True).first()
+
+        latest_result_qs = RaceData.objects.filter(race_id=latest_race_pk,
+                                                   type=RaceData.RACE_RESULT).order_by(
+            OrderBy(F("position_number"), nulls_last=True)).values_list(
+                "position_number", "driver__name", "constructor__name")[:3]
+
+        driver_standing_qs = SeasonDriverStanding.objects.filter(
+            year_id=settings.CURRENT_YEAR).order_by("position_number").select_related("driver")
+        team_standing_qs = SeasonConstructorStanding.objects.filter(
+            year_id=settings.CURRENT_YEAR).order_by("position_number").select_related("constructor")
+
+        race_qs = Race.objects.filter(year_id=settings.CURRENT_YEAR).select_related(
+            "grand_prix", "circuit").order_by("pk")
+
+        last_race_result = []
+        for position, driver_name, team_name in latest_result_qs:
+            temp = {
+                "position": position,
+                "driverName": driver_name,
+                "teamName": team_name,
+            }
+            last_race_result.append(temp)
+
+        races_list = []
+        next_race = {}
+        last_race = {}
+        for race in race_qs:
+            temp = {
+                "round": race.round,
+                "name": race.grand_prix.name,
+                "co_ordinates": race.circuit.cor_ordinates,
+                "date": race.event_date,
+            }
+
+            if not next_race and race.race_time > current_time:
+                next_race = {
+                    "name": race.grand_prix.name,
+                    "nextEvent": race.next_event[0],
+                    "nextEventType": race.next_event[1],
+                }
+
+            if latest_race_pk == race.pk:
+                last_race = {
+                    "round": race.round,
+                    "name": race.grand_prix.name,
+                }
+
+            races_list.append(temp)
+        context_dict['raceData'] = races_list
+        context_dict['nextRace'] = next_race
+
+        last_race['standings'] = last_race_result
+        context_dict['lastRaceResult'] = last_race
+
+        driver_standing_list = []
+        for standing in driver_standing_qs:
+            temp = {
+                "name": standing.driver.name,
+                "points": trim_decimal_zeros(standing.points),
+            }
+            driver_standing_list.append(temp)
+        context_dict['driverStanding'] = driver_standing_list
+
+        team_standing_list = []
+        for standing in team_standing_qs:
+            temp = {
+                "name": standing.constructor.name,
+                "points": trim_decimal_zeros(standing.points),
+            }
+            team_standing_list.append(temp)
+        context_dict['teamStanding'] = team_standing_list
+
+        if self.API_RESPONSE:
+            return JsonResponse(context_dict)
+
+        return render(request, self.TEMPLATE, context_dict)
